b773f590e226e088fb5fe191531c10f0fd9722fc
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2018-05-15 08:05:17 -0400

    Formatting

--- java/src/enchcracker/EnchData.java
+++ java/src/enchcracker/EnchData.java
@@ -8,15 +8,15 @@ public class EnchData{
   private int s2 = -1;
   private int s3 = -1;
 
-  public EnchData(int shelves,int s1,int s2,int s3){
-    this.shelves=shelves;
-    this.s1=s1;
-    this.s2=s2;
-    this.s3=s3;
+  public EnchData(int shelves, int s1, int s2, int s3){
+    this.shelves = shelves;
+    this.s1 = s1;
+    this.s2 = s2;
+    this.s3 = s3;
   }
 
   @Override
   public String toString(){
-    return String.format("%2d   bookshelves:     %2d     %2d     %2d%n",this.shelves,this.s1,this.s2,this.s3);
+    return String.format("%2d   bookshelves:     %2d     %2d     %2d%n", this.shelves, this.s1, this.s2, this.s3);
   }
 }
