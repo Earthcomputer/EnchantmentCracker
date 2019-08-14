754734b4baa2f5cff2c13cf96917bc92d495c5d2
Joe <burtonjae@hotmail.co.uk>
2019-08-14 23:36:22 +0100

    Fix enchantment order

--- src/enchcracker/Enchantments.java
+++ src/enchcracker/Enchantments.java
@@ -61,15 +61,46 @@ public class Enchantments {
 	private static final List<CompatibilityFunction> COMPATIBILITY_FUNCTIONS = new ArrayList<>();
 
 	static {
-		for (Field field : Enchantments.class.getFields()) {
-			if (Modifier.isStatic(field.getModifiers()) && Modifier.isFinal(field.getModifiers()) && field.getType() == String.class) {
-				try {
-					ALL_ENCHANTMENTS.add((String) field.get(null));
-				} catch (Exception e) {
-					throw new AssertionError(e);
-				}
-			}
-		}
+		// Add in registry order
+		Collections.addAll(ALL_ENCHANTMENTS,
+				PROTECTION,
+				FIRE_PROTECTION,
+				FEATHER_FALLING,
+				BLAST_PROTECTION,
+				PROJECTILE_PROTECTION,
+				RESPIRATION,
+				AQUA_AFFINITY,
+				THORNS,
+				DEPTH_STRIDER,
+				FROST_WALKER,
+				BINDING_CURSE,
+				SHARPNESS,
+				SMITE,
+				BANE_OF_ARTHROPODS,
+				KNOCKBACK,
+				FIRE_ASPECT,
+				LOOTING,
+				SWEEPING,
+				EFFICIENCY,
+				SILK_TOUCH,
+				UNBREAKING,
+				FORTUNE,
+				POWER,
+				PUNCH,
+				FLAME,
+				INFINITY,
+				LUCK_OF_THE_SEA,
+				LURE,
+				LOYALTY,
+				IMPALING,
+				RIPTIDE,
+				CHANNELING,
+				MULTISHOT,
+				QUICK_CHARGE,
+				PIERCING,
+				MENDING,
+				VANISHING_CURSE
+		);
 
 		// every enchantment with itself
 		COMPATIBILITY_FUNCTIONS.add((enchA, enchB, version) -> !enchA.equals(enchB));
