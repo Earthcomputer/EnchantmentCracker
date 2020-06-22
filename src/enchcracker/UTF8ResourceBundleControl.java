4d1abafa7b0c8f99a802fbef3752591cc3221524
Joe <burtonjae@hotmail.co.uk>
2020-06-22 13:15:03 +0100

    Fix ResourceBundle loading by adding escape sequences on load

--- src/enchcracker/UTF8ResourceBundleControl.java
+++ src/enchcracker/UTF8ResourceBundleControl.java
@@ -2,13 +2,13 @@ package enchcracker;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class UTF8ResourceBundleControl extends ResourceBundle.Control {
     @Override
@@ -34,12 +34,23 @@ public class UTF8ResourceBundleControl extends ResourceBundle.Control {
             stream = loader.getResourceAsStream(resourceName);
         }
         if (stream != null) {
-            try {
-                // Only this line is changed to make it to read properties files as UTF-8.
-                bundle = new PropertyResourceBundle(new InputStreamReader(stream, StandardCharsets.UTF_8));
-            } finally {
-                stream.close();
+            // This section is changed to allow for reading UTF-8. Non-ASCII characters are converted to escape sequences.
+            // Read input stream to string https://stackoverflow.com/a/5445161/11071180
+            String utf8Text;
+            try (Scanner scanner = new Scanner(stream, "UTF-8").useDelimiter("\\A")) {
+                utf8Text = scanner.hasNext() ? scanner.next() : "";
             }
+            // Replace non-ASCII with escape sequences
+            StringBuilder escapedText = new StringBuilder(utf8Text.length());
+            for (int i = 0; i < utf8Text.length(); i++) {
+                char c = utf8Text.charAt(i);
+                if (c < 128) {
+                    escapedText.append(c);
+                } else {
+                    escapedText.append(String.format("\\u%04x", (int)c));
+                }
+            }
+            bundle = new PropertyResourceBundle(new StringReader(escapedText.toString()));
         }
         return bundle;
     }
